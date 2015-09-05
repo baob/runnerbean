a1be8b34340bc1eed72279fd73c3718105300c52
baob <coder@onesandthrees.com>
2015-09-05 20:34:27 +0100

    passing test for kill command on single process

--- lib/test_process_automator/runner.rb
+++ lib/test_process_automator/runner.rb
@@ -5,5 +5,27 @@ module TestProcessAutomator
     def initialize(opts = {})
       @name = opts[:name] || 'tpa_runner'
     end
+
+    def add_process(opts)
+      opts.each do |key, value|
+        process_index[key] = value
+      end
+    end
+
+    def kill!(*process_names)
+      processes = process_names.map { |pn| process_from_name(pn) }
+      kills = processes.map(&:kill_command).compact
+      kills.map { |k| system(k) } unless kills.size == 0
+    end
+
+    private
+
+    def process_from_name(name)
+      process_index.fetch(name)
+    end
+
+    def process_index
+      @process_index ||= {}
+    end
   end
 end
--- spec/test_process_automator/runner_spec.rb
+++ spec/test_process_automator/runner_spec.rb
@@ -12,5 +12,23 @@ RSpec.describe TestProcessAutomator::Runner do
 
       specify { expect(subject).to be(name) }
     end
+
+    context 'and with a :frontend process injected' do
+      let(:the_frontend) { double('the frontend') }
+      before do
+        automator.add_process(frontend: the_frontend)
+      end
+
+      describe '#kill!(:frontend)' do
+        subject { automator.kill!(:frontend) }
+        let(:the_kill_command) { 'kill_that_frontend' }
+
+        it 'runs shell command specified for frontend' do
+          allow(the_frontend).to receive(:kill_command).and_return(the_kill_command)
+          expect(automator).to receive(:system).with(the_kill_command)
+          subject
+        end
+      end
+    end
   end
 end
