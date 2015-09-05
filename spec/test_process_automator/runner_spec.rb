819a4f0b15201d0e82fce4449ff929ff93a505ac
baob <coder@onesandthrees.com>
2015-09-05 23:03:52 +0100

    change terminology in spec

--- spec/test_process_automator/runner_spec.rb
+++ spec/test_process_automator/runner_spec.rb
@@ -2,7 +2,7 @@ require 'test_process_automator'
 require 'test_process_automator/runner'
 
 RSpec.describe TestProcessAutomator::Runner do
-  let(:automator) { described_class.new(init_options) }
+  let(:runner) { described_class.new(init_options) }
   let(:init_options) { { name: name } }
   let(:the_frontend_kill) { 'kill_that_frontend' }
   let(:the_frontend_start) { 'start_that_frontend' }
@@ -23,34 +23,34 @@ RSpec.describe TestProcessAutomator::Runner do
     let(:name) { 'a_name' }
 
     describe '#name' do
-      subject { automator.name }
+      subject { runner.name }
 
       specify { expect(subject).to be(name) }
     end
 
     context 'and with a :frontend process injected' do
-      before { automator.add_process(frontend: the_frontend) }
+      before { runner.add_process(frontend: the_frontend) }
 
       describe '#kill!(:frontend)' do
-        subject { automator.kill!(:frontend) }
+        subject { runner.kill!(:frontend) }
 
         it 'runs shell command specified for frontend' do
-          expect(automator).to receive(:system).with(the_frontend_kill)
+          expect(runner).to receive(:system).with(the_frontend_kill)
           subject
         end
       end
 
       describe '#start!(:frontend)' do
-        subject { automator.start!(:frontend) }
+        subject { runner.start!(:frontend) }
 
         it 'runs shell command specified for frontend' do
-          expect(automator).to receive(:system).with(the_frontend_start)
+          expect(runner).to receive(:system).with(the_frontend_start)
           subject
         end
       end
 
       describe '#start!(:backend)' do
-        subject { automator.start!(:backend) }
+        subject { runner.start!(:backend) }
 
         it 'raises not defined exception' do
           expect { subject }.to raise_error(::TestProcessAutomator::ProcessNotDefined)
@@ -58,33 +58,33 @@ RSpec.describe TestProcessAutomator::Runner do
       end
 
       context 'and with a :worker process injected' do
-        before { automator.add_process(worker: the_worker) }
+        before { runner.add_process(worker: the_worker) }
 
         describe '#kill!(:worker)' do
-          subject { automator.kill!(:worker) }
+          subject { runner.kill!(:worker) }
 
           it 'runs shell command specified for worker' do
-            expect(automator).to receive(:system).with(the_worker_kill)
+            expect(runner).to receive(:system).with(the_worker_kill)
             subject
           end
 
           it 'does not runs shell command specified for frontend' do
-            expect(automator).to_not receive(:system).with(the_frontend_kill)
+            expect(runner).to_not receive(:system).with(the_frontend_kill)
             subject
           end
         end
 
         describe '#kill!(:worker, :frontend)' do
-          subject { automator.kill!(:worker, :frontend) }
-          before { allow(automator).to receive(:system).with(anything) }
+          subject { runner.kill!(:worker, :frontend) }
+          before { allow(runner).to receive(:system).with(anything) }
 
           it 'runs shell command specified for worker' do
-            expect(automator).to receive(:system).with(the_worker_kill)
+            expect(runner).to receive(:system).with(the_worker_kill)
             subject
           end
 
           it 'runs shell command specified for frontend' do
-            expect(automator).to receive(:system).with(the_frontend_kill)
+            expect(runner).to receive(:system).with(the_frontend_kill)
             subject
           end
         end
