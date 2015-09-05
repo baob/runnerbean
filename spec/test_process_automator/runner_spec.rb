6d3442a165cf6e3bd6ddd22f22879af03fba5ab2
baob <coder@onesandthrees.com>
2015-09-05 21:23:53 +0100

    refactor specs

--- spec/test_process_automator/runner_spec.rb
+++ spec/test_process_automator/runner_spec.rb
@@ -3,6 +3,18 @@ require 'test_process_automator/runner'
 RSpec.describe TestProcessAutomator::Runner do
   let(:automator) { described_class.new(init_options) }
   let(:init_options) { { name: name } }
+  let(:the_frontend_kill) { 'kill_that_frontend' }
+  let(:the_frontend) do
+    double('the frontend',
+           kill_command: the_frontend_kill
+          )
+  end
+  let(:the_worker_kill) { 'kill_that_worker' }
+  let(:the_worker) do
+    double('the worker',
+           kill_command: the_worker_kill
+          )
+  end
 
   describe 'initalised with a name' do
     let(:name) { 'a_name' }
@@ -14,46 +26,30 @@ RSpec.describe TestProcessAutomator::Runner do
     end
 
     context 'and with a :frontend process injected' do
-      let(:the_frontend_kill_command) { 'kill_that_frontend' }
-      let(:the_frontend) do
-        double('the frontend',
-               kill_command: the_frontend_kill_command
-              )
-      end
-      before do
-        automator.add_process(frontend: the_frontend)
-      end
+      before { automator.add_process(frontend: the_frontend) }
 
       describe '#kill!(:frontend)' do
         subject { automator.kill!(:frontend) }
 
         it 'runs shell command specified for frontend' do
-          expect(automator).to receive(:system).with(the_frontend_kill_command)
+          expect(automator).to receive(:system).with(the_frontend_kill)
           subject
         end
       end
 
       context 'and with a :worker process injected' do
-        let(:the_worker_kill_command) { 'kill_that_worker' }
-        let(:the_worker) do
-          double('the worker',
-                 kill_command: the_worker_kill_command
-                )
-        end
-        before do
-          automator.add_process(worker: the_worker)
-        end
+        before { automator.add_process(worker: the_worker) }
 
         describe '#kill!(:worker)' do
           subject { automator.kill!(:worker) }
 
           it 'runs shell command specified for worker' do
-            expect(automator).to receive(:system).with(the_worker_kill_command)
+            expect(automator).to receive(:system).with(the_worker_kill)
             subject
           end
 
           it 'does not runs shell command specified for frontend' do
-            expect(automator).to_not receive(:system).with(the_frontend_kill_command)
+            expect(automator).to_not receive(:system).with(the_frontend_kill)
             subject
           end
         end
@@ -63,12 +59,12 @@ RSpec.describe TestProcessAutomator::Runner do
           before { allow(automator).to receive(:system).with(anything) }
 
           it 'runs shell command specified for worker' do
-            expect(automator).to receive(:system).with(the_worker_kill_command)
+            expect(automator).to receive(:system).with(the_worker_kill)
             subject
           end
 
           it 'runs shell command specified for frontend' do
-            expect(automator).to receive(:system).with(the_frontend_kill_command)
+            expect(automator).to receive(:system).with(the_frontend_kill)
             subject
           end
         end
